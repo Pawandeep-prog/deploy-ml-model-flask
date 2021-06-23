4b419c9a834384ff8716c09ba810e5723b4858ce
pawandeep-prog <singhpawandeep315@gmail.com>
2021-06-23 12:36:29 +0530

    updated to run with latest version;'

--- iris.py
+++ iris.py
@@ -9,7 +9,7 @@ y = np.array(df.iloc[:, 4:])
 
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
-y = le.fit_transform(y)
+y = le.fit_transform(y.reshape(-1))
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
